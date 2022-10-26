befeb86a73b91a1366022dbdfba87cf204bdd860
Sergey Evseev <sevseev@gmail.com>
2022-10-26 15:05:06 +0300

    Add project files.

---
+++ AbstractFactory(DoorFactory)/Program.cs
@@ -0,0 +1,105 @@
+﻿/*Фабричный метод(Factory Method) - это паттерн, который определяет интерфейс для создания объектов некоторого класса, 
+но непосредственное решение о том, объект какого класса создавать происходит в подклассах.То есть паттерн предполагает, 
+что базовый класс делегирует создание объектов классам-наследникам.
+Недостатки паттерна - для каждого нового продукта необходимо создавать свой класс создателя.*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractFactory_DoorFactory_
+{
+    interface Door
+    {        
+        string getDescription();
+    }
+
+    //класс деревенные двери
+    class WoodenDoor : Door
+    {
+        //private Door door;
+        public string getDescription()
+        {
+            return "Я деревянная дверь";
+        }
+    }
+    //класс железные двери
+    class IronDoor : Door
+    {
+        public string getDescription()
+        {
+            return "Я железная дверь";
+        }
+    }
+    //интерфейс установщика дверей
+    interface DoorFittingExpert
+    {
+        string getDescription();
+    }
+    //класс сварщика
+    class Welder : DoorFittingExpert
+    {
+        public string getDescription()
+        {
+            return "Я могу устанавливать металлические двери";
+        }
+    }
+    //класс плотника
+    class Carpenter : DoorFittingExpert
+    {
+        public string getDescription()
+        {
+            return "Я могу устанавливать деревянные двери";
+        }
+    }
+    //абстрактная фабрика
+    abstract class DoorFactory 
+    {
+        public abstract Door makeDoor();
+        public abstract DoorFittingExpert makeFittingExpert();
+    }
+    //класс деревянных дверей
+    class WoodenDoorDactory : DoorFactory
+    {
+        public override Door makeDoor()
+        {
+           return new WoodenDoor();
+        }
+
+        public override DoorFittingExpert makeFittingExpert()
+        {
+           return new Carpenter();
+        }
+    }
+    //класс железныйх дверей
+    class IronDoorFactory : DoorFactory
+    {
+        public override Door makeDoor()
+        {
+           return new IronDoor();
+        }
+
+        public override DoorFittingExpert makeFittingExpert()
+        {
+           return new Welder();
+        }
+    }
+
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DoorFactory factory = new WoodenDoorDactory();
+            Door door = factory.makeDoor();
+            door.getDescription();
+
+            DoorFittingExpert expert = factory.makeFittingExpert();
+            expert.getDescription();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ CarFactory/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarFactory
+{
+    //интерфейс двигателя
+    interface IEngine
+    {
+        void ReleaseEngine();
+    }
+    //конкретный класс двигателя
+    class JapaneseEngine : IEngine
+    {
+        public void ReleaseEngine() => Console.WriteLine("Японский двигатель");
+    }
+    //конкретный класс двигателя
+    class RussianEngine : IEngine
+    {
+        public void ReleaseEngine() => Console.WriteLine("Российский двигатель");
+    }
+    //интерфейс автомобиля
+    interface ICar
+    {
+        void ReleaseCar(IEngine engine); //принимает ссылку на интерфейс двигатеоля
+    }
+    //класс конкретного автомобиля
+    class JapaneseCar : ICar
+    {
+        public void ReleaseCar(IEngine engine)
+        {
+            Console.Write("Собрали японский автомобиль: ");
+            engine.ReleaseEngine();
+        }
+    }
+    //класс конкретного автомобиля
+    class RussianCar : ICar
+    {
+        public void ReleaseCar(IEngine engine)
+        {
+            Console.Write("Собрали российский автомобиль: ");
+            engine.ReleaseEngine();
+        }
+    }
+    //интерфейс фабрики
+    interface IFactory
+    {
+        IEngine createEngine(); //метод создающий двигатель
+        ICar createCar(); //метод создающий автомобиль
+    }
+    //класс конкретной японской фабрики
+    class JapaneseFactory : IFactory
+    {
+        public ICar createCar() => new JapaneseCar();
+        public IEngine createEngine() => new JapaneseEngine();
+    }
+    //класс конкретной российской фабрики
+    class RussianFactory : IFactory
+    {
+        public ICar createCar() => new RussianCar(); //создает и возвращает экземпляр автомобиля
+        public IEngine createEngine() => new RussianEngine();
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //построение японской фабрики
+            JapaneseFactory jFactory = new JapaneseFactory();
+            //создание японского двигателя
+            IEngine jEngine = jFactory.createEngine();
+            //создание японского автомобилля
+            ICar jCar = jFactory.createCar();
+            jCar.ReleaseCar(jEngine);
+
+
+            //постороение российской фабрики 
+            RussianFactory rFactory = new RussianFactory();
+            //создание российкого двигателя
+            IEngine rEngine = rFactory.createEngine();
+            //создание росс. автомобиля
+            ICar rCar = rFactory.createCar();
+            //выпуск россиского автомобиля
+            rCar.ReleaseCar(rEngine);
+
+
+        }
+    }
+}
